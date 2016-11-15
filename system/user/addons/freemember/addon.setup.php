66633068e83aedc6f94e94158ade8c28147d409a
Jarrod Nix <nix.jarrod@gmail.com>
2016-11-15 18:24:26 -0500

    Begin conversion for EE3

---
+++ system/user/addons/freemember/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'author'      		=> 'Reusser Design',
+	'author_url'  		=> 'http://reusserdesign.com',
+	'docs_url'			=> 'https://github.com/reusserdesign/freemember',
+	'name'        		=> 'FreeMember',
+	'description' 		=> 'ExpressionEngine3 member templating plugin, forked from devdemon/freemember.',
+	'version'     		=> '3.0.0',
+	'namespace'   		=> 'freemember',
+	'settings_exist'	=> FALSE
+);
